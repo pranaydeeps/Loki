44961ffb4477fcabe7d6cf7470128e70725655d1
c1pher <pranaydeeps@gmail.com>
2022-03-11 12:00:13 +0100

    IDK MAN WHY DO I HAVE TO WRITE A COMMIT MSG EVERYTIME

--- Scripts/baseline_multiple_choice.py
+++ Scripts/baseline_multiple_choice.py
@@ -127,19 +127,18 @@ def compute_metrics(pred):
 
 training_args = TrainingArguments(
      output_dir=".",
-     evaluation_strategy="steps",
-     eval_steps=200,
-     learning_rate=1e-5,
+     evaluation_strategy="epoch",
+     learning_rate=5e-5,
      per_device_train_batch_size=2,
      per_device_eval_batch_size=2,
-     num_train_epochs=3,
+     num_train_epochs=5,
      gradient_accumulation_steps=4,
      weight_decay=0.01,
      save_strategy='no')
 
 
 
-trainer = CustomTrainer(
+trainer = Trainer(
      model=model,
      args=training_args,
      train_dataset=tokenized_data["train"],
